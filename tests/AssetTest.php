45915e8442eb40a7506c62a6d1851ef733f7ca45
Dave James Miller <dave@davejamesmiller.com>
2020-02-23 14:26:23 +0000

    Add AssetTest

---
+++ tests/AssetTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MigrationsUITests;
+
+class AssetTest extends TestCase
+{
+    public function listOfAssets()
+    {
+        return [
+            ['app.css', 'text/css'],
+            ['app.js', 'application/javascript'],
+            ['favicon.png', 'image/png'],
+        ];
+    }
+
+    /** @dataProvider listOfAssets */
+    public function testAsset($name, $type)
+    {
+        $response = $this->get("/migrations/assets/$name");
+
+        $response->assertOk();
+        $response->assertHeader('Content-Type', $type);
+
+        $this->expectOutputString(file_get_contents(__DIR__ . "/../build/$name"));
+        $response->sendContent();
+    }
+
+    public function testMissingFile()
+    {
+        $this
+            ->withExceptionHandling()
+            ->get('/migrations/assets/invalid.css')
+            ->assertNotFound();
+    }
+
+    public function testUnsafeFilename()
+    {
+        $this
+            ->withExceptionHandling()
+            ->get('/migrations/assets/../composer.json')
+            ->assertNotFound();
+    }
+}
