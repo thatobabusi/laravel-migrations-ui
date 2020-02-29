339bae377bdc7a47958307eaaaa4d553f81b2417
Dave James Miller <dave@davejamesmiller.com>
2020-02-29 15:34:21 +0000

    Fix Artisan tests in Laravel 5.6

--- tests/Commands/ListPathsTest.php
+++ tests/Commands/ListPathsTest.php
@@ -2,23 +2,40 @@
 
 namespace MigrationsUITests\Controllers;
 
+use Illuminate\Support\Facades\Artisan;
 use MigrationsUITests\TestCase;
+use Mockery;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class ListPathsTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Laravel 5.6 didn't include the Artisan testing helpers (expectsOutput, assertExitCode)
+        if (method_exists($this, 'withoutMockingConsoleOutput')) {
+            $this->withoutMockingConsoleOutput();
+        }
+    }
+
     public function testDefaultResponse()
     {
-        $this->artisan('migrations-ui:list-paths')
-            ->expectsOutput('["' . database_path('migrations') . '"]')
-            ->assertExitCode(0);
+        $output = Mockery::mock(BufferedOutput::class . '[writeln]');
+        $output->shouldReceive('writeln')->once()->with('["' . database_path('migrations') . '"]', Mockery::any());
+
+        $exitCode = Artisan::call('migrations-ui:list-paths', [], $output);
+        $this->assertSame(0, $exitCode);
     }
 
     public function testCustomPath()
     {
         app('migrator')->path('/custom/path');
 
-        $this->artisan('migrations-ui:list-paths')
-            ->expectsOutput('["/custom/path","' . database_path('migrations') . '"]')
-            ->assertExitCode(0);
+        $output = Mockery::mock(BufferedOutput::class . '[writeln]');
+        $output->shouldReceive('writeln')->once()->with('["/custom/path","' . database_path('migrations') . '"]', Mockery::any());
+
+        $exitCode = Artisan::call('migrations-ui:list-paths', [], $output);
+        $this->assertSame(0, $exitCode);
     }
 }
