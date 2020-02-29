c5e468f868e4fb129ec9464c14fb63bfa52546a7
Dave James Miller <dave@davejamesmiller.com>
2020-02-29 17:10:37 +0000

    Fix tests in Laravel 5.8 and below

--- tests/Repositories/MigrationsRepositoryTest.php
+++ tests/Repositories/MigrationsRepositoryTest.php
@@ -5,6 +5,7 @@ namespace MigrationsUITests\Repositories;
 use DaveJamesMiller\MigrationsUI\Repositories\MigrationsRepository;
 use Illuminate\Support\Facades\App;
 use MigrationsUITests\TestCase;
+use Mockery;
 
 class MigrationsRepositoryTest extends TestCase
 {
@@ -30,7 +31,11 @@ class MigrationsRepositoryTest extends TestCase
         // invoke Artisan in a separate process - but see ListPathsTest for
         // where we do test that case.
 
-        App::partialMock()->expects('runningInConsole')->andReturn(false);
+        // Note: App::partialMock() is not available in Laravel 5.8 and below
+
+        $mock = Mockery::mock(app());
+        $mock->shouldReceive('runningInConsole')->andReturn(false);
+        App::swap($mock);
 
         $repo = app(MigrationsRepository::class);
 
